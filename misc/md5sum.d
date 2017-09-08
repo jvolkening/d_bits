f2d32bc979222da2f689e77807dbe3f9e2d938cc
jvolkening <jdv@base2bio.com>
2017-09-08 09:35:45 -0500

    clean up carryover code

--- misc/md5sum.d
+++ misc/md5sum.d
@@ -12,30 +12,13 @@ enum VERSION = 0.001;
 
 int main(string[] args) {
 
-    ulong lineCount;
-    ulong wordCount;
-    ulong charCount;
-    ulong byteCount;
-    ulong maxLine;
-
-    bool print_l; // newlines
-    bool print_w; // words
-    bool print_c; // characters
-    bool print_b; // bytes
-    bool print_m; // longest line
-
     bool vers;
 
     // parse command-line arguments
     auto opts = getopt( args,
         std.getopt.config.caseSensitive,
         std.getopt.config.bundling,
-        "l|lines",           "print the newline counts",             &print_l,
-        "w|words",           "print the word counts",                &print_w,
-        "m|chars",           "print the character counts",           &print_c,
-        "c|bytes",           "print the byte counts",                &print_b,
-        "L|max-line-length", "print the length of the longest line", &print_m,
-        "version",           "output version information and exit",  &vers,
+        "version", "output version information and exit", &vers,
     );
 
     // print help message
@@ -48,13 +31,6 @@ int main(string[] args) {
     if (vers) {
         printVersion();
         return(0);
-    }
-
-    // default is to pring word, line, and byte counts
-    if ( !( print_b || print_c || print_l || print_w || print_m) ) {
-        print_l = true;
-        print_w = true;
-        print_b = true;
     }
 
     // the rest of arguments should be filenames
@@ -81,13 +57,8 @@ int main(string[] args) {
 void printOpts( Option[] opts ) {
 
     writeln(q"[
-Usage: wc [OPTION]... [FILE]...
-Print newline, word, and byte counts for each FILE, and a total line if
-more than one FILE is specified.  With no FILE, or when FILE is -,
-read standard input.  A word is a non-zero-length sequence of characters
-delimited by white space.
-The options below may be used to select which counts are printed, always in
-the following order: newline, word, character, byte, maximum line length.
+Usage: md5sum [OPTION]... [FILE]...
+Print or check MD5 (128-bit) checksums.
 ]");
 
     foreach (opt; opts) {
@@ -97,7 +68,7 @@ the following order: newline, word, character, byte, maximum line length.
 
     writeln(q"[
 
-Full documentation at: <http://foo.bar/wc>
+Full documentation at: <http://foo.bar/md5sum>
 ]");
 
 }
